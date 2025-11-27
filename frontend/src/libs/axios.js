4cfea63a2101b636ce05fa342960e0eef2359de7
ikhzanwordpress <ikhzanwordpress@gmail.com>
2025-11-27 12:02:18 +0700

    todolist

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.VUE_APP_API_URL || "http://localhost:8080",
+    baseURL: process.env.VUE_APP_API_URL || "http://todo-backend:8080",
 });
